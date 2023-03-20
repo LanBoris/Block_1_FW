710dfbb49d7ea39c2b5faed98736dcb1c15b556b
Boris Lanovenko <blanovenko@gmail.com>
2023-03-20 23:26:12 +0300

    Create FillArrray

--- finalProject/Program.cs
+++ finalProject/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите количество вводимых слов: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] arrayOfStrings = new string[size];
+
+
+void FillArray(string[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"Введите {i+1} элемент массива: ");
+    array[i] = Console.ReadLine();
+  }
+}
+
+FillArray(arrayOfStrings);
+Console.WriteLine(String.Join(", ", arrayOfStrings));
+
